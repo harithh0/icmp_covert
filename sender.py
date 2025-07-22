25a517d28bc681e41f3485f54a750caf8ad04083
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-07-21 19:50:48 -0500

    added somethings

--- sender.py
+++ sender.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import threading
 
@@ -6,11 +7,16 @@ from prompt_toolkit.patch_stdout import patch_stdout
 from scapy.all import *
 
 session = PromptSession()
+# Silence only Scapy‐runtime warnings (leave other logs intact)
+logging.getLogger("scapy.runtime").setLevel(
+    logging.ERROR)  # or logging.CRITICAL
 
 
 def sniff_icmp():
 
     def handle_recv(packet):
+        if packet[ICMP].type == 0:
+            return
         print_formatted_text("Message recieved:", packet[Raw].load)
 
     fil = "src host 10.0.0.212 and icmp"
@@ -19,20 +25,28 @@ def sniff_icmp():
 
 def handle_message():
     data_bytes = bytes([0x01] * 40)
-    data = "hello"
+    while True:
+        data = session.prompt(">")
+        key = 0x55
+        full = "".join([str(hex(ord(c) ^ key)) for c in data])
 
-    data = session.prompt(">")
-    key = 0x55
-    full = "".join([str(hex(ord(c) ^ key)) for c in data])
+        # the id comes from the current process id and doing a Internet checksum (used to identify the "session" or "process" sending the ping)
+        icmp_id = os.getpid() & 0xFFFF
+        # seq increments per sent packet, helps detect packet loss, track individual requests
+        icmp_seq = 1
 
-    # the id comes from the current process id and doing a Internet checksum (used to identify the "session" or "process" sending the ping)
-    icmp_id = os.getpid() & 0xFFFF
-    # seq increments per sent packet, helps detect packet loss, track individual requests
-    icmp_seq = 1
-
-    icmp_packet = IP(dst="10.0.0.212") / ICMP(id=icmp_id,
-                                              seq=icmp_seq) / Raw(load=full)
-    send(icmp_packet)
+        icmp_packet = (IP(dst="10.0.0.212") / ICMP(id=icmp_id, seq=icmp_seq) /
+                       Raw(load=data))
+        counter = 0
+        status = False
+        while status is False:
+            resp = sr1(icmp_packet, verbose=0, timeout=3)
+            if counter > 3:
+                break
+            if resp:
+                status = True
+                break
+            counter += 1
 
 
 sniffing_thread = threading.Thread(target=sniff_icmp)
